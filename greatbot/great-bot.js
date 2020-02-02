89678bfc50b9e55e5da4eeba3d57dd74e62796e2
Suraj Rai <surajrajrai@gmail.com>
2020-02-02 22:33:20 +0000

    Update great-bot.js

--- greatbot/great-bot.js
+++ greatbot/great-bot.js
@@ -1,43 +1,43 @@
 var botui = new BotUI('great-bot');
+var company = "initial";
+var website = "initial";
 
 //starting point for journey
-botui.message.add({
-  content: "Hello, what's your main goal today?"
-}).then(function () {
-  return botui.action.button({
-    delay: 1000,
-    action: [{
-      text: 'Just explore',
-      value: 'explore'
-    }, {
-      text: 'Find an opportunity',
-      value: 'findopportunity'
-    },{
-      text: 'Learn about exporting',
-      value: 'Learn'
-    },{
-      text: 'Find a new market',
-      value: 'findmarket'
-    },{
-      text: 'Tell us more about your business',
-      value: 'moreaboutyourbusiness'
-    },{
-      text: 'Other',
-      value: 'other'
-    }]
-  })
-}).then(function (res) {
-  if (res.value === 'other') {
-    other()
-  } else if (res.value === 'findopportunity') {
-    findopportunity()
-  } else if (res.value === 'moreaboutyourbusiness') {
-    moreaboutyourbusiness()
-  }
-});
-
-function moreaboutyourbusiness() {
-
+function init() {
+  botui.message.add({
+    content: "Hello, what's your main goal today?"
+  }).then(function () {
+    return botui.action.button({
+      delay: 1000,
+      action: [{
+        text: 'Just explore',
+        value: 'explore'
+      }, {
+        text: 'Find an opportunity',
+        value: 'findopportunity'
+      },{
+        text: 'Learn about exporting',
+        value: 'Learn'
+      },{
+        text: 'Find a new market',
+        value: 'findmarket'
+      },{
+        text: 'Link your business to your profile',
+        value: 'moreaboutyourbusiness'
+      },{
+        text: 'Other',
+        value: 'other'
+      }]
+    })
+  }).then(function (res) {
+    if (res.value === 'other') {
+      other()
+    } else if (res.value === 'findopportunity') {
+      findopportunity()
+    } else if (res.value === 'moreaboutyourbusiness') {
+      moreaboutyourbusiness()
+    }
+  });
 }
 
 //Top level function for OTHER journey option
@@ -138,11 +138,169 @@ function other() {
     action: [{
       text: 'Tariff finder',
       value: 'tarriffinder'
+    },{
+      text: 'Report a trade barrier',
+      value: 'reporttradebarrier'
+    },{
+      text: 'Get an EORI number',
+      value: 'getandEORInumber'
     }]
     })
   })
 };
 //Top level function for moreaboutyourbusiness journey option
-function moreaboutyourbusiness() {
-
+function moreaboutyourbusiness(){
+  botui.message.add({
+    delay:1000,
+    human: true,
+    content: "Can you tell us your business's registered company name?"
+  }).then(function() {
+    return botui.action.text({
+      action:{
+        placeholder: 'Company name'
+      }
+    });
+  }).then(function (res){
+    company = res.value; // savecompany
+    botui.message.add({
+      delay: 1000,
+      content: 'Is this your company?'
+    })
+  }).then(function (res){
+    botui.message.add({
+      delay: 1000,
+      content: "Registered company name: " + company
+    })
+  }).then(function (){
+    botui.message.add({
+      delay: 1000,
+      content: "Company number: 12354473"
+    })
+  }).then(function (){
+    botui.message.add({
+      delay: 1000,
+      content: "Nature of business: Educational support services"
+    })
+  }).then(function (){
+    botui.message.add({
+      delay: 1000,
+      content: "Incorporated on: 09 January 2020"
+    })
+  }).then(function (){
+    botui.message.add({
+      delay: 1000,
+      content: "Address: Windsor House, 18R3, SW90AD"
+    })
+  }).then(function (){
+    return botui.action.button({
+      delay:2000,
+      action: [{
+        text: 'Yes',
+        value: 'yesmore'
+      }, {
+      text: 'No',
+      value: 'nomore'
+      }]
+    })
+  }).then(function (res) {
+   if (res.value === 'yesmore' ) {
+     yesrightbusiness()
+   } else if (res.value === 'nomore'){
+     moreaboutyourbusiness()
+   }
+  })
 }
+
+//function for yes correct business
+function yesrightbusiness() {
+  botui.message.add({
+    delay:1000,
+    content: "Can you share your business's website if it has one?"
+  }).then(function (){
+  botui.message.add({
+    delay: 1000,
+    content: "The website address must start with http:// or https://"
+  })
+}).then(function (){
+  return botui.action.button({
+    delay:1000,
+    action: [{
+      text: 'I have a business website',
+      value: 'businesswebsite'
+    }, {
+    text: "I'd like to skip or add later",
+    value: 'nobusinesswebsite'
+    }]
+  })
+}).then(function (res) {
+ if (res.value === 'businesswebsite' ) {
+   businesswebsite()
+ } else if (res.value === 'nobusinesswebsite'){
+   nobusinesswebsite()
+ }
+})
+}
+
+function businesswebsite (){
+  botui.message.add({
+    delay:1000,
+    human: true,
+    content: "What is your business's registered website?"
+  }).then(function() {
+    return botui.action.text({
+      action:{
+        placeholder: 'https://www.example.com'
+      }
+    });
+  }).then(function (res){
+    website = res.value; // savecompany
+    botui.message.add({
+      delay: 1000,
+      content: 'Thank you, these are your now your linked details'
+    })
+  }).then(function (res){
+    botui.message.add({
+      delay: 2000,
+      content: "Registered company name: " + company
+    })
+  }).then(function (){
+    botui.message.add({
+      delay: 2000,
+      content: "Company number: 12354473"
+    })
+  }).then(function (){
+    botui.message.add({
+      delay: 2000,
+      content: "Address: Windsor House, 18R3, SW90AD"
+    })
+  }).then(function (){
+    botui.message.add({
+      delay: 2000,
+      content: "Webite: " + website
+    })
+  }).then(function (){
+    botui.message.add({
+      delay: 2000,
+      content: "Was there anything else we could help with?"
+    })
+  }).then(function (){
+    return botui.action.button({
+      delay:3000,
+      action: [{
+        text: 'Yes',
+        value: 'yesmore'
+      }, {
+      text: 'No',
+      value: 'nomore'
+      }]
+    })
+  }).then(function (res) {
+   if (res.value === 'yesmore' ) {
+     yesmore()
+   } else if (res.value === 'nomore'){
+     nomore()
+   }
+  })
+};
+
+init();
