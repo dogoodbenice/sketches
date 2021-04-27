dd5aa4013c1f9deb4a59e9880f47b3b023abdc6f
Suraj Rai <surajrajrai@gmail.com>
2021-04-27 11:53:20 +0100

    new iteration

--- g-art-system-3/iteration1/sketch.js
+++ g-art-system-3/iteration1/sketch.js
@@ -1,61 +1,22 @@
-const CRYSTAL_SIZE = 150
-const SIDES = 6
-
-//LAYOUT IN THE GRID
-const MARGIN = CRYSTAL_SIZE / 2
-const COLUMNS = 4
-const ROWS = 4
-const PADDING = CRYSTAL_SIZE * 0.2
-const GRIDBOX = CRYSTAL_SIZE + PADDING
-const START = (CRYSTAL_SIZE / 2) + MARGIN
-
-let PALETTE = []
-ALL_CRYSTALS = []
-const layers = []
-
 function setup() {
-  const totalX = START + GRIDBOX * COLUMNS
-  const totalY = START + GRIDBOX * ROWS
-  createCanvas(totalX, totalY)
-  console.log(totalX)
-  console.log(totalY)
-
-  //This is our set of colours, amend these to define something new
-  PALETTE = [
-    color('red'), // red
-    color('yellow'), // yellow
-    color('blue') // blue
-  ]
-
-  noLoop()
-  angleMode(DEGREES)
-  rectMode(CENTER)
+    createCanvas(800, 800)
+    angleMode(DEGREES)
+    noLoop()
 }
 
 function draw() {
-  for (var x = 0; x < COLUMNS; x++) {
-    for (var y = 0; y < ROWS; y++) {
-      const posX = START + (x * GRIDBOX)
-      const posY = START + (y * GRIDBOX)
-      new Crystal(posX,posY)
-      ALL_CRYSTALS.push(new Crystal(posX, posY))
-      background(255,255,255)
+    background(40)
+    noFill()
+    stroke(255)
+
+    translate(width / 4, height / 3)
+    // const x = cos(animLoop.theta) * width / 3
+    // const y = animLoop.noise() * height / 3
+    // ellipse(x, y, 50, 50)
+
+    for (var i = 0; i < 500; i++) {
+      var z = sin(frameCount) * (int(random(5,500)))
+      triangle(200, z, 100, 300, 300, 300);
     }
-  }
 
-  ALL_CRYSTALS.forEach(Crystal => {
-    Crystal.render()
-  });
-
-}
-
-function mouseClicked() {
-  clear()
-  draw()
-}
-
-function keyTyped() {
-  if (key === 's') {
-    saveCanvas();
-  }
 }
