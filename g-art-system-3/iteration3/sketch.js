62dfeaa087df1adbad237caa391509ebc64a11da
Suraj Rai <surajrajrai@gmail.com>
2021-04-30 17:57:42 +0100

    Update sketch.js

--- g-art-system-3/iteration3/sketch.js
+++ g-art-system-3/iteration3/sketch.js
@@ -1,36 +1,36 @@
-//var size = 5;
+let angle = 0;
 
 function setup() {
-  createCanvas(500, 500);
-  colorMode(RGB)
+  createCanvas(500, 500, WEBGL);
   angleMode(DEGREES);
-  frameRate(100)
-  noStroke()
+  frameRate(40)
   createLoop({
-       // gif: {
-       //     options: { quality: 5 },
-       //     fileName: "noiseLoop.gif",
-       //    startLoop: 1,
-       //     endLoop: 2
-       // }
+       gif: {
+           options: { quality: 6 },
+           fileName: "noiseLoop.gif",
+          startLoop: 1,
+           endLoop: 2
+       }
   })
 }
 
 
 function draw() {
-  background(255);
-  var size = sin(frameCount) * 45
+  background(255,50);
+  var size = sin(frameCount) * 20;
   var m = 20;
-  fill(animLoop.progress, 0.5, 1)
+  angle += 0.05;
+
+
   for (var i = 0; i < m; i++) {
     for (var j = 0; j < m; j++) {
       var x = map(i, 0, m - 1, 0, width);
       var y = map(j, 0, m - 1, 0, height);
-      //var size = periodicFunction(t-offset(x,y)); // the important line of code
-      //strokeWeight(size);
-      //point(x, y);
-      rect(y+size, x+size, size)
+      strokeWeight(size);
+      rotateZ(angle);
+      //rotateY(angle);
+      point(x, y);
+      rect(y, x, size)
     }
   }
-  console.log(size)
 }
