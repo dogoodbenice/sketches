5afc43281537e70cbbe0c22ae2555cb6496cab87
Suraj Rai <surajrajrai@gmail.com>
2021-05-01 19:39:01 +0100

    Update sketch.js

--- g-art-system-3/iteration5/sketch.js
+++ g-art-system-3/iteration5/sketch.js
@@ -1,15 +1,15 @@
 let angle = 0;
 
 function setup() {
-  createCanvas(500, 500, WEBGL);
+  createCanvas(500, 500);
   angleMode(DEGREES);
   frameRate(40)
-  //noStroke();
+  noStroke();
   createLoop({
        // gif: {
        //     options: { quality: 6 },
-       //     fileName: "noiseLoop.gif",
-       //    startLoop: 0,
+       //     fileName: "loop.gif",
+       //    startLoop: 1,
        //     endLoop: 2
        // }
   })
@@ -17,26 +17,23 @@ function setup() {
 
 
 function draw() {
-  background(255);
-  //var size = sin(frameCount) * 15;
-  var m = 50;
-  angle += 3;
-  fill(255, 204, 0);
-  //for (var i = 0; i < m; i++) {
-    //for (var j = 0; j < m; j++) {
-      // var x = map(i, 0, m - 1, 0, width);
-      // var y = map(j, 0, m - 1, 0, height);
-      rotateZ(angle);
-      rotateX(angle);
-      rotateY(angle);
-      //rect(y, x, size)
-      //ellipse(y, x, size)
-      // point(10, 75, 20)
-      // point(50, 175, 20)
-      // point(100, 50, 20)
-      // point(20, 50, 20)
-      triangle(30, 75, 58, 20, 86, 75);
-    //}
-  //}
-  //console.log(angle)
+  background(0,10);
+  var size = sin(frameCount) * 15;
+  var m = 2;
+  angle += 2;
+  fill(0,100,200)
+  //fill(255, 204, 0); yellow
+  translate(width / 2, height / 2);
+  for (var i = 0; i < m; i++) {
+    for (var j = 0; j < m; j++) {
+      rotate(angle);
+      ellipse(20, 50, size)
+      ellipse(50, 100, size)
+      ellipse(75, 80, size)
+      ellipse(175, 80, size)
+      //triangle(30, 75, 58, 20, 86, 75);
+      rect(30,75,20)
+      rect(60,175,20)
+    }
+  }
 }
