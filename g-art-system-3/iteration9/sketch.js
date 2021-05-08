95764f3019e283d88f48a9abb79976ebf54f52c8
Suraj Rai <surajrajrai@gmail.com>
2021-05-08 18:52:35 +0100

    Update sketch.js

--- g-art-system-3/iteration9/sketch.js
+++ g-art-system-3/iteration9/sketch.js
@@ -6,14 +6,14 @@ function setup() {
   frameRate(20)
   noFill()
   createLoop({
-    duration: 15,
-       gif: {
-           options: { quality: 30},
-           fileName: "ttriangle.gif",
-           download: true,
-           startLoop: 1,
-           endLoop: 2
-       }
+    // duration: 15,
+    //    gif: {
+    //        options: { quality: 30},
+    //        fileName: "ttriangle.gif",
+    //        download: true,
+    //        startLoop: 1,
+    //        endLoop: 2
+    //    }
    })
 }
 
@@ -28,15 +28,10 @@ function draw() {
         stroke('#E3DE44')
         triangle(30, 75, 58, 20, 86, 75);
         ellipse(58,20,10);
-
         triangle(30, 175, 58, 120, 86, 175);
         ellipse(158,120,10);
-
         triangle(30, 275, 58, 220, 86, 275);
-
-
         triangle(30, 375, 58, 320, 86, 375);
-
         ellipse(158,320,10);
         triangle(30, 475, 58, 420, 86, 475);
         ellipse(58,420,10);
