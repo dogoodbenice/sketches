fc9cc13f6a030efb8e2eb767bc411b0fdf68e1e7
Suraj Rai <surajrajrai@gmail.com>
2021-05-08 18:40:26 +0100

    updates

--- g-art-system-3/iteration9/sketch.js
+++ g-art-system-3/iteration9/sketch.js
@@ -9,7 +9,7 @@ function setup() {
     duration: 15,
        gif: {
            options: { quality: 30},
-           fileName: "tumble.gif",
+           fileName: "ttriangle.gif",
            download: true,
            startLoop: 1,
            endLoop: 2
@@ -18,15 +18,29 @@ function setup() {
 }
 
 function draw() {
-  background('#FBFEF9');
-  var m = 10;
-  let angle = sin(frameCount) * 11
-  translate(-130, 0);
+  background('#3C3F8C');
+  var m = 3;
+  let angle = sin(frameCount) * 200
+  translate(0, -250);
   for (var i = 0; i < m; i++) {
     for (var j = 0; j < m; j++) {
-      rotateX(angle);
-        stroke('#A63446')
-        rect(30*j,40*i,30)
+      rotateY(angle);
+        stroke('#E3DE44')
+        triangle(30, 75, 58, 20, 86, 75);
+        ellipse(58,20,10);
+
+        triangle(30, 175, 58, 120, 86, 175);
+        ellipse(158,120,10);
+
+        triangle(30, 275, 58, 220, 86, 275);
+
+
+        triangle(30, 375, 58, 320, 86, 375);
+
+        ellipse(158,320,10);
+        triangle(30, 475, 58, 420, 86, 475);
+        ellipse(58,420,10);
     }
   }
+  //console.log(angle);
 }
