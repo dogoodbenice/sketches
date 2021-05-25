61271a3b56db718c819bd5a074f40e2b4cba148e
Suraj Rai <surajrajrai@gmail.com>
2021-05-25 20:45:53 +0100

    Update sketch.js

--- g-art-system-4/iteration11/sketch.js
+++ g-art-system-4/iteration11/sketch.js
@@ -1,7 +1,5 @@
 function setup() {
   createCanvas(1000,1000);
-  angleMode(DEGREES);
-  frameRate(10)
   rectMode(CENTER)
   grid1();
   noStroke();
@@ -11,54 +9,63 @@ function mousePressed() {
   grid1();
 }
 
-// function keyPressed() {
-//   saveCanvas('cgrid', 'jpg');
-// }
+function keyPressed() {
+  saveCanvas('blokgrid', 'jpg');
+}
 
 function grid1() {
-  const topColor = color('#DDB4F6');
-  const bottomColor = color('#8DD0FC');
+  const topColor = color('#F3696E');
+  const bottomColor = color('#F8A902');
   for (let y = 0; y < 1000; y++) {
     const lineColor = lerpColor(topColor, bottomColor, y / height);
     stroke(lineColor);
     line(0, y, width, y);
-    //line(0, y, height/2, y);
   }
-  let w = 60;
+  let w = 40;
   let h = w;
 
   for (let x = 0; x <= width + w; x += w) { // columns
     for (let y = 0; y <= height + h; y += h) { // rows
       strokeWeight(4);
-      tsize = 60
-      let r = random(6);
+      tsize = 40
+      let r = random(5);
 
       if (r < 1) {
         // diagonal top-left to bottom-right
-        //rect(x,y,tsize)
+        // push()
+        // noStroke();
+        // fill('#2720E8')
+        // triangle(x + tsize / 2, y + tsize / 2, x - tsize / 2, y - tsize / 2, x - tsize / 2, y + tsize / 2);
+        // pop()
       } else if (r < 2) {
         // diagonal top-right to bottom-left
         //line(x + w, y, x, y + h);
         noStroke();
-        fill('#fcb98d')
+        fill('#0251f8')
         rect(x, y, tsize)
+        triangle(x + tsize / 2, y + tsize / 2, x - tsize / 2, y - tsize / 2, x - tsize / 2, y + tsize / 2);
       } else if (r < 3) {
         // straight across top
-        // fill('#ef9cda')
-        // rect(x,y,tsize)
-      } else if (r < 4) {
-        //triangle(x-tsize, y+tsize, x+tsize, y+tsize, x+tsize, y+tsize);
         push()
         noStroke();
-        fill('#fc8d99')
+        fill('#125A99')
+        rect(x, y, tsize)
+        fill('#4F2078')
         triangle(x + tsize / 2, y + tsize / 2, x - tsize / 2, y - tsize / 2, x - tsize / 2, y + tsize / 2);
         pop()
+      } else if (r < 4) {
+        //triangle(x-tsize, y+tsize, x+tsize, y+tsize, x+tsize, y+tsize);
+        // push()
+        // noStroke();
+        // fill('#385CB0')
+        // triangle(x + tsize / 2, y + tsize / 2, x - tsize / 2, y - tsize / 2, x - tsize / 2, y + tsize / 2);
+        // pop()
       } else {
-        push()
-        noStroke();
-        fill('#fcf18d')
-        triangle(x + tsize / 2, y + tsize / 2, x - tsize / 2, y + tsize / 2, x + tsize / 2, y - tsize / 2);
-        pop()
+        // push()
+        // noStroke();
+        // fill('#02317D')
+        // triangle(x + tsize / 2, y + tsize / 2, x - tsize / 2, y + tsize / 2, x + tsize / 2, y - tsize / 2);
+        // pop()
       }
 
     }
