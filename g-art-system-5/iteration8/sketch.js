54355d96496bda7e587155d558132298d25500d6
Suraj Rai <surajrajrai@gmail.com>
2021-06-09 21:49:11 +0100

    Update sketch.js

--- g-art-system-5/iteration8/sketch.js
+++ g-art-system-5/iteration8/sketch.js
@@ -1,10 +1,10 @@
 var speed;
 
 function setup() {
-  createCanvas(1000, 1000);
-  speed = random(10,20)
+  createCanvas(400, 400);
+  speed = 20
   createLoop({
-    duration: 3,
+    duration: 4,
        gif: {
            options: { quality: 30},
            fileName: "topgraphy.gif",
@@ -20,24 +20,24 @@ function setup() {
 // }
 
 function draw() {
-  background(30);
+  background('#1B1464');
   motion();
 }
 
 function motion() {
-  var size = 40;
-  var count = 10;
+  var size = 30;
   translate(20, 20);
-  for (var x = 5; x < 20; x++) {
-    for (var y = 5; y < 20; y++) {
+  for (var x = 3; x < 10; x++) {
+    for (var y = 3; y < 10; y++) {
       push();
       translate(x * size, y * size);
-      fill(255, 250, 255, 20);
+      fill(255, 250, 255,10);
       stroke(255, 10);
       rotate((frameCount / speed + x / 0.01 + y / 1));
       strokeWeight(3);
       stroke(255);
       ellipse(size/3, size/3, 10)
+      //ellipse(size/3, size/3, 5)
       pop();
     }
   }
