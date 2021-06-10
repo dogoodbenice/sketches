4d0c00209fd8ac0097f7de9e08630a9976d7e17a
Suraj Rai <surajrajrai@gmail.com>
2021-06-10 22:03:12 +0100

    Update sketch.js

--- g-art-system-5/iteration9/sketch.js
+++ g-art-system-5/iteration9/sketch.js
@@ -1,8 +1,8 @@
 var speed;
 
 function setup() {
-  createCanvas(1000, 1000, WEBGL);
-  speed = random(10,20)
+  createCanvas(1000, 1000);
+  speed = 2
   // createLoop({
   //   duration: 6,
   //      gif: {
@@ -14,7 +14,7 @@ function setup() {
   //      }
   //  })
 }
-
+//
 function keyPressed() {
   saveCanvas('movement', 'jpg');
 }
@@ -28,21 +28,23 @@ function draw() {
 }
 
 function motion() {
-  var size = 40;
+  var size = 50;
   var count = 10;
-  translate(-20, -20);
-  for (var x = 0; x < 5; x++) {
-    for (var y = 0; y < 5; y++) {
+  for (var x = 5; x < 17; x++) {
+    for (var y = 5; y < 17; y++) {
       push();
       translate(x * size, y * size);
       //fill(255, 250, 255, 20);
       stroke(255, 10);
-      rotateZ((frameCount / x / 0.001 + y / 1));
-      rect(size/20, size/20, 10)
+      rotate((frameCount / x / speed + y / 1));
+      rect(size/20, size/20, 2.5)
+      rect(size/20, size/2, 2.5)
+      rect(size/2, size/2, 2.5)
+      rect(size/2, size/20, 2.5)
       strokeWeight(3);
       stroke(255);
       //rotateX((frameCount / speed + x / 0.01 + y / 1));
-      rect(size/3, size/3, 10)
+      ellipse(size/3, size/3, 10)
       pop();
     }
   }
