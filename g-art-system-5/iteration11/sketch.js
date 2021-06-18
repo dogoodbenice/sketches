91429ab447b80934075d9a099528d18188ab830e
Suraj Rai <surajrajrai@gmail.com>
2021-06-18 21:13:34 +0100

    sketches

--- g-art-system-5/iteration11/sketch.js
+++ g-art-system-5/iteration11/sketch.js
@@ -2,43 +2,48 @@ var speed;
 
 function setup() {
   createCanvas(500, 500);
-  // createLoop({
-  //   duration: 6,
-  //      gif: {
-  //          options: { quality: 30},
-  //          fileName: "topgraphy.gif",
-  //          download: true,
-  //          startLoop: 1,
-  //          endLoop: 2
-  //      }
-  //  })
+  createLoop({
+    duration: 7,
+       gif: {
+           options: { quality: 30},
+           fileName: "motion.gif",
+           download: true,
+           startLoop: 1,
+           endLoop: 2
+       }
+   })
 }
 
-function keyPressed() {
-  saveCanvas('movement', 'jpg');
-}
+// function keyPressed() {
+//   saveCanvas('movement', 'jpg');
+// }
 
 function draw() {
-  background(30);
+  background('#000F08');
   motion();
   //drawGrid();
   //triangle(60, 20, 100, 60, 60, 60);
+  //ellipse(80,40,10);
+  //triangle(100, 20, 100, 60, 60, 20);
 }
 
 function motion() {
   var space = 40;
-  var count = 6;
+  var count = 5;
   speed = 50
   translate(150, 150);
   for (var x = 0; x < count; x++) {
     for (var y = 0; y < count; y++) {
       push();
       translate(x * space, y * space);
-      fill(255,200);
-      stroke(255,50);
-      strokeWeight(3);
-      rotate((frameCount / speed + x / 0.02));
+      fill('#8F2D56');
+      noStroke();
+      rotate((frameCount / speed + (y+x) / 0.00005));
       triangle(60, 20, 100, 60, 60, 60);
+      fill("#D81159");
+      triangle(100, 20, 100, 60, 60, 20);
+      fill('#FBB13C')
+      ellipse(80,40,15);
       pop();
     }
   }
---
+++ g-art-system-5/iteration14/sketch.js
@@ -0,0 +1,66 @@
+var t=1;
+
+function setup() {
+  createCanvas(500, 500);
+  // createLoop({
+  //   duration: 6,
+  //      gif: {
+  //          options: { quality: 30},
+  //          fileName: "topgraphy.gif",
+  //          download: true,
+  //          startLoop: 1,
+  //          endLoop: 2
+  //      }
+  //  })
+}
+
+// function keyPressed() {
+//   saveCanvas('movement', 'jpg');
+// }
+
+function draw() {
+  background(30);
+  stroke(255,100);
+  strokeWeight(2);
+  t+=0.75;
+  motion()
+}
+
+function motion() {
+  var space = 55;
+  var count = 6;
+  speed = 50
+  translate(110, 200);
+  for (var x = 0; x < count; x++) {
+    //for (var y = 0; y < count; y++) {
+      push();
+      translate(x * space, space);
+      fill(255,200);
+      stroke(255,50);
+      strokeWeight(3);
+      rotate((frameCount / speed + x / 0.02));
+      for (var i = 0; i < 50; i++) {
+        line(x1(t+i),y1(t+i),x2(t+i),y2(t+i))
+        point(x1(t+i),y1(t+i))
+        point(x2(t+i),y2(t+i))
+      }
+      pop();
+    //}
+  }
+}
+
+function x1(t) {
+  return sin(t/10) * 50 + sin(t/15)*20;
+}
+
+function y1(t) {
+  return cos(t/10)*50;
+}
+
+function x2(t) {
+  return sin(t/10) * 25 + sin(t/15)*25;
+}
+
+function y2(t) {
+  return cos(t/10)*100;
+}
