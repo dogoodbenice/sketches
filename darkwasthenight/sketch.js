684dd9f7bd6fed96bf3da1f36277fe7781bf5611
Suraj Rai <surajrajrai@gmail.com>
2022-03-27 12:34:32 +0100

    updates to old sketches

--- darkwasthenight/sketch.js
+++ darkwasthenight/sketch.js
@@ -7,10 +7,7 @@ function draw() {
   background(0);
   fill(255, 242, 124);
   ellipse(mouseX, height/2, 100, 100);
-  filter(BLUR);
-  fill(255, 255, 0);
-  ellipse(mouseX, height/2, 90, 90);
   fill(0);
   textSize(32);
-  text("DARK WAS THE NIGHT", 100, height/2);
+  text("DARK WAS THE NIGHT", 100, height/1.95);
 }
