c0af297613e44dbe7f1edc3ab5f07dd903c99fd9
Suraj Rai <surajrajrai@gmail.com>
2022-12-23 17:20:00 +0000

    file changes

--- genuary2023/10/sketch.js
+++ genuary2023/10/sketch.js
@@ -1,3 +1,12 @@
+import * as Tone from 'tone'
+
+//create a synth and connect it to the main output (your speakers)
+const synth = new Tone.Synth().toDestination();
+
+//play a middle 'C' for the duration of an 8th note
+synth.triggerAttackRelease("C4", "8n");
+
+
 // An array to store the positions of the letters and have the letters
 let positions = [];
 let items = ['L', 'O', 'V', 'E'];
