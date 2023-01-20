e3beb97faf71310074359ba863dff972ec2b45a3
Suraj Rai <surajrajrai@gmail.com>
2023-01-20 16:42:36 +0000

    sketch

--- genuary2023/20/sketch.js
+++ genuary2023/20/sketch.js
@@ -1,55 +1,66 @@
-// An array to store the positions of the letters and have the letters
-let positions = [];
-let items = ['L', 'O', 'V', 'E'];
-let letterSize = 30;
-
-function random_item(items) {
-  return items[Math.floor(Math.random() * items.length)];
-}
-
 function setup() {
-  createCanvas(400, 400);
-  background(255, 50, 25);
-  textSize(letterSize);
-  // Draw lots of letters
-  for (let i = 0; i < 500; i++) {
-    fill(255);
-    // Set a random rotation angle
-    let angle = random(-180, 180);
-    rotate(angle);
-    // Set a random position on the canvas
-    let x = random(letterSize*2, width-letterSize);
-    let y = random(letterSize*2, height-letterSize);
-    // Check if the new position overlaps with any existing letters
-    let overlaps = false;
-    for (let i = 0; i < positions.length; i++) {
-      let p = positions[i];
-      let d = dist(x, y, p.x, p.y);
-      if (d < textWidth("L")) {
-        overlaps = true;
-        break;
-      } else if (d < textWidth("O")){
-        overlaps = true;
-        break;
-      } else if (d < textWidth("V")){
-        overlaps = true;
-        break;
-      } else if (d < textWidth("E")){
-        overlaps = true;
-        break;
-      }  
-    }
-    // If the position does not overlap, draw the letter and store the position
-    if (!overlaps) {
-      text(random_item(items), x, y);
-      positions.push({ x, y });
-    }
-    // Reset the rotation
-    rotate(-angle);
-  }
+  createCanvas(600, 600);
+  noFill();
+  noLoop();  // Disable looping, we will draw the patterns only once
 }
 
-function mouseClicked() {
-  // Save as an image
-  save("letters.jpg");
+function draw() {
+  // Set the background color to white
+  background(27, 40, 74);
+  // Set the fill color to a random color
+  noFill();
+  strokeWeight(4)
+  stroke(195, 157, 57)
+
+
+  // Set the size of each cell in the grid
+  let cellSize = width / 10;
+
+  let pattern = int(random(3));
+
+  switch (pattern) {
+    case 0:
+      // Generate and draw the unique patterns in each cell of the grid
+      for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+          //left triangle
+          triangle(cellSize * i + cellSize * 0.1, cellSize * j + cellSize * 0.1,
+            cellSize * (i + 0.1), cellSize * j + cellSize * 0.9,
+            cellSize * (i + 1) - cellSize * 0.5, cellSize * j + cellSize * 0.9);
+          //centre triangle
+          triangle(cellSize * i + cellSize * 0.1, cellSize * j + cellSize * 0.9,
+            cellSize * (i + 0.5), cellSize * j + cellSize * 0.1,
+            cellSize * (i + 1) - cellSize * 0.1, cellSize * j + cellSize * 0.9);
+          //right triangle
+          triangle(cellSize * i + cellSize * 0.5, cellSize * j + cellSize * 0.9,
+            cellSize * (i + 0.9), cellSize * j + cellSize * 0.1,
+            cellSize * (i + 1) - cellSize * 0.1, cellSize * j + cellSize * 0.9);
+
+
+          // rect(cellSize * i + cellSize * 0.1, cellSize * j + cellSize * 0.1, cellSize * 0.5);
+          // rect(cellSize * i + cellSize * 0.1, cellSize * j + cellSize * 0.3, cellSize * 0.5);
+          // rect(cellSize * i + cellSize * 0.3, cellSize * j + cellSize * 0.3, cellSize * 0.5);
+          // rect(cellSize * i + cellSize * 0.3, cellSize * j + cellSize * 0.1, cellSize * 0.5);
+
+
+          //     line(cellSize * i + cellSize * 0.1, cellSize * j + cellSize * 0.1,
+          //       cellSize * (i + 1) - cellSize * 0.1, cellSize * (j + 1) - cellSize * 0.1);
+          //  line(cellSize * i + cellSize * 0.1, cellSize * (j + 1) - cellSize * 0.1,
+          //       cellSize * (i + 1) - cellSize * 0.1, cellSize * j + cellSize * 0.1);
+
+        }
+      }
+      break;
+    case 1:
+      break;
+    case 2:
+      break;
+  }
+
+}
+
+function keyPressed() {
+  if (key === 's') {
+    saveCanvas('stampset', 'jpg');
+  }
 }
