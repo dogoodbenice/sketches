0e329c3b8b2db3385e6264212dc278bc308c4843
Suraj Rai <surajrajrai@gmail.com>
2023-01-21 21:31:16 +0000

    Update sketch.js

--- genuary2023/21/sketch.js
+++ genuary2023/21/sketch.js
@@ -1,55 +1,76 @@
-// An array to store the positions of the letters and have the letters
-let positions = [];
-let items = ['L', 'O', 'V', 'E'];
-let letterSize = 30;
-
-function random_item(items) {
-  return items[Math.floor(Math.random() * items.length)];
-}
+let tileSize;
+let rows, cols;
+let color1, color2, color3, color4, color5, color6;
+let circleRadius;
 
 function setup() {
-  createCanvas(400, 400);
-  background(255, 50, 25);
-  textSize(letterSize);
-  // Draw lots of letters
-  for (let i = 0; i < 500; i++) {
-    fill(255);
-    // Set a random rotation angle
-    let angle = random(-180, 180);
-    rotate(angle);
-    // Set a random position on the canvas
-    let x = random(letterSize*2, width-letterSize);
-    let y = random(letterSize*2, height-letterSize);
-    // Check if the new position overlaps with any existing letters
-    let overlaps = false;
-    for (let i = 0; i < positions.length; i++) {
-      let p = positions[i];
-      let d = dist(x, y, p.x, p.y);
-      if (d < textWidth("L")) {
-        overlaps = true;
-        break;
-      } else if (d < textWidth("O")){
-        overlaps = true;
-        break;
-      } else if (d < textWidth("V")){
-        overlaps = true;
-        break;
-      } else if (d < textWidth("E")){
-        overlaps = true;
-        break;
-      }  
-    }
-    // If the position does not overlap, draw the letter and store the position
-    if (!overlaps) {
-      text(random_item(items), x, y);
-      positions.push({ x, y });
-    }
-    // Reset the rotation
-    rotate(-angle);
-  }
+  createCanvas(600, 600);
+  circleRadius = random(100,150);
+  stripeCount = 20;
+  stripeThickness = 20;
+  tileSize = 30;
+  rows = floor(height / tileSize);
+  cols = floor(width / tileSize);
+  color1 = color(166,54,58);
+  color2 = color(67,90,129);
+  color3 = color(234,208,187);
+  color4 = color(141,109,91);
+  color5 = color(random(0,255), random(0,128), random(0,255));
+  color6 = color(random(0,255), random(0,128), random(0,255));
+  rectMode(CENTER)
 }
 
-function mouseClicked() {
-  // Save as an image
-  save("letters.jpg");
+function draw() {
+  background(240);
+  
+  // Draw the tiles
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      let x = j * tileSize;
+      let y = i * tileSize;
+      let c = (i + j) % 2 === 0 ? color1 : color2;
+      fill(c);
+      noStroke();
+      rect(x, y, tileSize, 10);
+      let d = (i + j) % 2 === 0 ? color3 : color4;
+      fill(d);
+      rect(x, y+10, tileSize, 10);
+      fill(c);
+      triangle(x+10,y+10,x+10,y+10,x+10,y+10);
+      let e = (i + j) % 2 === 0 ? color5 : color6;
+      fill(e);
+      rect(x, y+20, tileSize, 10);
+      ellipse(x, y+10, tileSize, 10);
+    }
+  }
+  
+  //cover the other areas
+  fill(240);
+  noStroke();
+  rect(width/2, 0, 600, 40);
+  rect(width/2, 600, 600, 40);
+  rect(50, height/2, 100, 600);
+  rect(550, height/2, 100, 600);
+
+  //draw the border
+  strokeWeight(10);
+  stroke(50);
+  noFill();
+  rect(width/2, height/2, 400, 550);
+  
+  // Draw the rug hem
+  fill(245)
+  rect(width/2, 85, 300, 20);
+  rect(width/2, 515, 300, 20);
+  
+  // Draw the rug 
+  noFill();
+  rect(width/2, height/2, 300, 450);
+  ellipse(width/2, height/2, circleRadius*2);
+}
+
+function keyPressed() {
+  if (key === 's') {
+    saveCanvas('rug', 'jpg');
+  }
 }
