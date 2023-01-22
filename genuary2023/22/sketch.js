837ec2a9661cfafaf47355a9287ca1c88c836260
Suraj Rai <surajrajrai@gmail.com>
2023-01-22 16:30:10 +0000

    genuary

--- genuary2023/22/sketch.js
+++ genuary2023/22/sketch.js
@@ -1,55 +1,49 @@
-// An array to store the positions of the letters and have the letters
-let positions = [];
-let items = ['L', 'O', 'V', 'E'];
-let letterSize = 30;
-
-function random_item(items) {
-  return items[Math.floor(Math.random() * items.length)];
-}
+let rotatespeed;
+let r, g, b;
 
 function setup() {
-  createCanvas(400, 400);
-  background(255, 50, 25);
-  textSize(letterSize);
-  // Draw lots of letters
-  for (let i = 0; i < 500; i++) {
-    fill(255);
-    // Set a random rotation angle
-    let angle = random(-180, 180);
-    rotate(angle);
-    // Set a random position on the canvas
-    let x = random(letterSize*2, width-letterSize);
-    let y = random(letterSize*2, height-letterSize);
-    // Check if the new position overlaps with any existing letters
-    let overlaps = false;
-    for (let i = 0; i < positions.length; i++) {
-      let p = positions[i];
-      let d = dist(x, y, p.x, p.y);
-      if (d < textWidth("L")) {
-        overlaps = true;
-        break;
-      } else if (d < textWidth("O")){
-        overlaps = true;
-        break;
-      } else if (d < textWidth("V")){
-        overlaps = true;
-        break;
-      } else if (d < textWidth("E")){
-        overlaps = true;
-        break;
-      }  
-    }
-    // If the position does not overlap, draw the letter and store the position
-    if (!overlaps) {
-      text(random_item(items), x, y);
-      positions.push({ x, y });
-    }
-    // Reset the rotation
-    rotate(-angle);
-  }
+  createCanvas(600, 600, WEBGL);
+  noStroke();
+  smooth();
+  rotatespeed = random(2000,10000)
+  r = random(0,255)
+  g = random(0,255)
+  b = random(0,255)
 }
 
-function mouseClicked() {
-  // Save as an image
-  save("letters.jpg");
+function draw() {
+  background(0);
+  ambientLight(255);
+
+  pointLight(r, g, b, 0, 0, 0);
+  specularMaterial(250);
+  shininess(25);
+
+  torus(60, 12, 50, 20);
+  
+  rotateY(millis() / rotatespeed);
+  torus(30, 12, 50, 20);
+
+  rotateY(millis() / rotatespeed*1.2);  
+  torus(90, 12, 50, 20);
+
+  rotateY(millis() / rotatespeed*1.3);
+  torus(120, 12, 150, 120);
+  
+  rotateY(millis() / rotatespeed*1.4);
+  torus(150, 12, 150, 120);
+
+  
+  rotateY(millis() / rotatespeed*1.5);
+  torus(180, 12, 150, 120);
+
+  rotateY(millis() / rotatespeed*1.6);
+  torus(210, 12, 150, 120);
+}
+
+function keyPressed() {
+  // this will download the first 3 seconds of the animation!
+  if (key === 's') {
+    saveGif('mySketch', 3.5);
+  }
 }
