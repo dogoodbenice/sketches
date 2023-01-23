425d7a023a76e0bcf3e3346004f181cba1fd0844
Suraj Rai <surajrajrai@gmail.com>
2023-01-23 19:34:57 +0000

    genuary

--- genuary2023/23/sketch.js
+++ genuary2023/23/sketch.js
@@ -1,55 +1,46 @@
-// An array to store the positions of the letters and have the letters
-let positions = [];
-let items = ['L', 'O', 'V', 'E'];
-let letterSize = 30;
-
-function random_item(items) {
-  return items[Math.floor(Math.random() * items.length)];
-}
+let count;
+let angle2 = 0; 
+let num; 
+let scalar = 70;
 
 function setup() {
-  createCanvas(400, 400);
-  background(255, 50, 25);
-  textSize(letterSize);
-  // Draw lots of letters
-  for (let i = 0; i < 500; i++) {
-    fill(255);
-    // Set a random rotation angle
-    let angle = random(-180, 180);
-    rotate(angle);
-    // Set a random position on the canvas
-    let x = random(letterSize*2, width-letterSize);
-    let y = random(letterSize*2, height-letterSize);
-    // Check if the new position overlaps with any existing letters
-    let overlaps = false;
-    for (let i = 0; i < positions.length; i++) {
-      let p = positions[i];
-      let d = dist(x, y, p.x, p.y);
-      if (d < textWidth("L")) {
-        overlaps = true;
-        break;
-      } else if (d < textWidth("O")){
-        overlaps = true;
-        break;
-      } else if (d < textWidth("V")){
-        overlaps = true;
-        break;
-      } else if (d < textWidth("E")){
-        overlaps = true;
-        break;
-      }  
-    }
-    // If the position does not overlap, draw the letter and store the position
-    if (!overlaps) {
-      text(random_item(items), x, y);
-      positions.push({ x, y });
-    }
-    // Reset the rotation
-    rotate(-angle);
+  createCanvas(600, 600);
+  rectMode(CENTER)
+  frameRate(20)
+  count = int(random(20,30))
+  num = int(random(1,10))
+}
+
+function draw() {  
+  background(255);
+  noFill();
+  circlePattern(width/2,height/2);
+  let ang2 = radians(angle2);
+  let y2 = height / 2 + scalar * sin(ang2);
+  squarePattern(width/2,height/2)
+  circlePattern(width/2,y2);
+  for (var i = 0; i < count; i++){    
+    circlePattern(width/2*i,height/2)
+  }
+  angle2 += num;
+}
+
+function circlePattern(x,y){
+  strokeWeight(4);
+  for (var i = 0; i < count; i++){    
+    ellipse(x,y,20*i);
   }
 }
 
-function mouseClicked() {
-  // Save as an image
-  save("letters.jpg");
+function squarePattern(x,y){
+  strokeWeight(4);
+  for (var i = 0; i < count; i++){    
+    rect(x,y,20*i);
+  }
+}
+
+function keyPressed() {
+  if (key === 's') {
+    saveGif('moire', 5);
+  }
 }
