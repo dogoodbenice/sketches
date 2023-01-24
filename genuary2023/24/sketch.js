26de2ce88ae676c47a9a617b55bbbbfb7719b456
Suraj Rai <surajrajrai@gmail.com>
2023-01-24 21:56:11 +0000

    genuary

--- genuary2023/24/sketch.js
+++ genuary2023/24/sketch.js
@@ -1,8 +1,6 @@
 
 function setup() {
   createCanvas(600, 600);
-  xoff = 0;
-  yoff = 50;
   rectMode(CENTER)
   fill(random(0, 255), random(0, 255), random(0, 255));
   noStroke();
@@ -13,19 +11,22 @@ function setup() {
 function textile() {
   fill(random(0, 255), random(0, 255), random(0, 255));
   noStroke();
-  rect(width / 2, height / 2, 400)
-  stroke(1)
+  rect(width / 2, height / 2, 500)
+  fill(random(0, 255), random(0, 255), random(0, 255));
+  noStroke();
+  ellipse(width / 2, height / 2, 300)
+  stroke(random(1,80))
   for (x = 0; x < (width); x++) {
-    for (y = 0; y < height - yoff; y++) {
-      var n = noise(x * 0.02, y * 0.02);
-      if (random(1) > 0.9 - 0.01 * 1 - n / 10) {
+    for (y = 0; y < (height); y++) {
+      let n = noise(x * 2, y * 2);
+      if (random(1) > 0.9 - (random(0.001,0.5)) * 1-n /10) {
         strokeWeight(
           random(
-            0.2 + y / 500 - n / 10,
+            0.1 + y / 500 - n / 10,
             0.3 + y / 100 - n / 10 - 1 / 5
           )
         );
-        point(x + 1 * (width - yoff * 2) / width + random(-2, 2), yoff+y+random(-6, 100));
+        point(x + random(-2, 2), y+ random(-0, 100));
       }
     }
   }
