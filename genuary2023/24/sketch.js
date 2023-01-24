0f76884eb25348a93a9dfe1657bad9058d5d4804
Suraj Rai <surajrajrai@gmail.com>
2023-01-24 21:25:55 +0000

    genuary

--- genuary2023/24/sketch.js
+++ genuary2023/24/sketch.js
@@ -1,55 +1,40 @@
-// An array to store the positions of the letters and have the letters
-let positions = [];
-let items = ['L', 'O', 'V', 'E'];
-let letterSize = 30;
-
-function random_item(items) {
-  return items[Math.floor(Math.random() * items.length)];
-}
 
 function setup() {
-  createCanvas(400, 400);
-  background(255, 50, 25);
-  textSize(letterSize);
-  // Draw lots of letters
-  for (let i = 0; i < 500; i++) {
-    fill(255);
-    // Set a random rotation angle
-    let angle = random(-180, 180);
-    rotate(angle);
-    // Set a random position on the canvas
-    let x = random(letterSize*2, width-letterSize);
-    let y = random(letterSize*2, height-letterSize);
-    // Check if the new position overlaps with any existing letters
-    let overlaps = false;
-    for (let i = 0; i < positions.length; i++) {
-      let p = positions[i];
-      let d = dist(x, y, p.x, p.y);
-      if (d < textWidth("L")) {
-        overlaps = true;
-        break;
-      } else if (d < textWidth("O")){
-        overlaps = true;
-        break;
-      } else if (d < textWidth("V")){
-        overlaps = true;
-        break;
-      } else if (d < textWidth("E")){
-        overlaps = true;
-        break;
-      }  
+  createCanvas(600, 600);
+  xoff = 0;
+  yoff = 50;
+  rectMode(CENTER)
+  fill(random(0, 255), random(0, 255), random(0, 255));
+  noStroke();
+  rect(width / 2, height / 2, 600)
+  gradient()
+}
+
+function gradient() {
+
+  
+  fill(random(0, 255), random(0, 255), random(0, 255));
+  noStroke();
+  rect(width / 2, height / 2, 400)
+  stroke(1)
+  for (x = 0; x < (width); x++) {
+    for (y = 0; y < height - yoff; y++) {
+      var n = noise(x * 0.02, y * 0.02);
+      if (random(1) > 0.9 - 0.01 * 1 - n / 10) {
+        strokeWeight(
+          random(
+            0.2 + y / 500 - n / 10,
+            0.3 + y / 100 - n / 10 - 1 / 5
+          )
+        );
+        point(x + 1 * (width - yoff * 2) / width + random(-2, 2), yoff+y+random(-6, 100));
+      }
     }
-    // If the position does not overlap, draw the letter and store the position
-    if (!overlaps) {
-      text(random_item(items), x, y);
-      positions.push({ x, y });
-    }
-    // Reset the rotation
-    rotate(-angle);
   }
 }
 
-function mouseClicked() {
-  // Save as an image
-  save("letters.jpg");
+function keyPressed() {
+  if (key === 's') {
+    saveCanvas('rug', 'jpg');
+  }
 }
