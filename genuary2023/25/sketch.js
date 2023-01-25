fef8160b2b63fefd30d5a4bf02ffaf2486c22d8c
Suraj Rai <surajrajrai@gmail.com>
2023-01-25 21:59:11 +0000

    genuary

--- genuary2023/25/sketch.js
+++ genuary2023/25/sketch.js
@@ -1,55 +1,72 @@
-// An array to store the positions of the letters and have the letters
-let positions = [];
-let items = ['L', 'O', 'V', 'E'];
-let letterSize = 30;
-
-function random_item(items) {
-  return items[Math.floor(Math.random() * items.length)];
-}
+let size;
 
 function setup() {
-  createCanvas(400, 400);
-  background(255, 50, 25);
-  textSize(letterSize);
-  // Draw lots of letters
-  for (let i = 0; i < 500; i++) {
-    fill(255);
-    // Set a random rotation angle
-    let angle = random(-180, 180);
-    rotate(angle);
-    // Set a random position on the canvas
-    let x = random(letterSize*2, width-letterSize);
-    let y = random(letterSize*2, height-letterSize);
-    // Check if the new position overlaps with any existing letters
-    let overlaps = false;
-    for (let i = 0; i < positions.length; i++) {
-      let p = positions[i];
-      let d = dist(x, y, p.x, p.y);
-      if (d < textWidth("L")) {
-        overlaps = true;
-        break;
-      } else if (d < textWidth("O")){
-        overlaps = true;
-        break;
-      } else if (d < textWidth("V")){
-        overlaps = true;
-        break;
-      } else if (d < textWidth("E")){
-        overlaps = true;
-        break;
-      }  
-    }
-    // If the position does not overlap, draw the letter and store the position
-    if (!overlaps) {
-      text(random_item(items), x, y);
-      positions.push({ x, y });
-    }
-    // Reset the rotation
-    rotate(-angle);
-  }
+  createCanvas(550, 550, WEBGL);
+  fill(120);
+  size = random(10, 40);
 }
 
-function mouseClicked() {
-  // Save as an image
-  save("letters.jpg");
+function draw() {
+  background(0);
+  let radius = width * 1.5;
+  orbitControl(1, 1, 0.1);
+  specularMaterial(250);
+  shininess(5);
+  let lightPosX = mouseX - width / 2;
+  let lightPosY = mouseY - height / 2;
+  pointLight(200, 200, 200, lightPosX, lightPosY, 50);
+  ambientLight(60);
+
+  translate(0, 0, -600);
+  for (let i = 0; i <= 120; i++) {
+    for (let j = 0; j <= 120; j++) {
+      push();
+      let a = (j / 100) * PI;
+      let b = (i / 12) * PI;
+      translate(
+        sin(2 * a) * radius * sin(b),
+        (cos(b) * radius) / 3,
+        cos(2 * a) * radius * sin(b)
+      );
+      sphere(5, 5, 60);
+      pop();
+    }
+  }
+
+  for (let i = 0; i <= 120; i++) {
+    for (let j = 0; j <= 120; j++) {
+      push();
+      let a = (j / 10) * PI;
+      let b = (i / 100) * PI;
+      translate(
+        sin(2 * a) * radius * sin(b),
+        (cos(b) * radius) / 2,
+        cos(2 * a) * radius * sin(b)
+      );
+      sphere(5, 5, 60);
+      pop();
+    }
+  }
+
+  for (let i = 0; i <= 10; i++) {
+    for (let j = 0; j <= 10; j++) {
+      push();
+      let a = (j / 20) * PI;
+      let b = (i / 20) * PI;
+      translate(
+        sin(2 * a) * radius * sin(b),
+        (cos(b) * radius) / size,
+        cos(2 * a) * radius * sin(b)
+      );
+      sphere(5, 5, 60);
+      pop();
+    }
+  }
+
+}
+
+function keyPressed() {
+  if (key === 's') {
+    saveCanvas('kusamaview', 'jpg');
+  }
 }
