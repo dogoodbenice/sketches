4c74765bc859c78b1cff4930ef2917d9a5c0c779
Suraj Rai <surajrajrai@gmail.com>
2023-01-27 20:47:11 +0000

    genuary

--- genuary2023/27/sketch.js
+++ genuary2023/27/sketch.js
@@ -8,7 +8,7 @@ function setup() {
   createCanvas(600, 600);
   ellipseMode(CENTER);
   circleRadius = 150;
-  stripeCount = int(random(6,15));
+  stripeCount = int(random(12,20));
   stripeThickness = 20;
   stripeLength = circleRadius - stripeThickness / 15;
   stripeColors = [color(212, 164, 178), color(251,250,240), color(30,48,124), color(143,133,197), color(39,105,56), color(240,227,99), color(232,186,62), color(213,122,43), color(2,1,8), color(127,125,142), color(222,211,205), color(0, 255, 255), color(0, 0, 255), color(255, 0, 255), color(255, 0, 0), color(255, 255, 255), color(255, 200, 99), color(255, 255, 0), color(90, 255, 240), color(99, 227, 255), color(240,227,99)];
@@ -24,11 +24,11 @@ function draw() {
   fill(255);
   arc(width/2, height/2, 300, 300, PI + PI, PI, CHORD);
   
-  stroke(230);
-  fill(145);
+  noStroke();
+  fill(217,179,86);
   ellipse(width / 2, height / 2, 200);
-  fill(145);
-  arc(width/2, height/2, 250, 250, PI + PI, PI, CHORD);
+  fill(110);
+  arc(width/2, height/2, 200, 200, PI + PI, PI, CHORD);
   
   // Draw the stripes
   for (let i = 0; i < stripeCount; i++) {
