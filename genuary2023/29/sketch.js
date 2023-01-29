8f2e5178d89a0bad37ca1d37163645613a9cf358
Suraj Rai <surajrajrai@gmail.com>
2023-01-29 14:51:28 +0000

    genuary

--- genuary2023/29/sketch.js
+++ genuary2023/29/sketch.js
@@ -1,7 +1,7 @@
 // An array to store the positions of the letters and have the letters
 let positions = [];
 let items = ['L', 'O', 'V', 'E'];
-let letterSize = 30;
+let letterSize = 5;
 
 function random_item(items) {
   return items[Math.floor(Math.random() * items.length)];
@@ -10,16 +10,27 @@ function random_item(items) {
 function setup() {
   createCanvas(600, 600);
   background(255, 50, 25);
+  frame1(30, 30, 150);
+}
+
+function frame1(xpos, ypos, dimensions) {
+  //frame
+  fill(230);
+  stroke(0);
+  strokeWeight(15);
+  rect(xpos, ypos, dimensions)
+  //contents
+  noFill();
+  stroke(255);
   textSize(letterSize);
   // Draw lots of letters
-  for (let i = 0; i < 500; i++) {
+  for (let i = 0; i < 10; i++) {
     fill(255);
-    // Set a random rotation angle
     let angle = random(-180, 180);
     rotate(angle);
-    // Set a random position on the canvas
-    let x = random(letterSize*2, width-letterSize);
-    let y = random(letterSize*2, height-letterSize);
+    // Set a random position within the rect
+    let x = random(xpos-(dimensions+dimensions), xpos+dimensions);
+    let y = random(ypos-(dimensions+dimensions), ypos+dimensions);
     // Check if the new position overlaps with any existing letters
     let overlaps = false;
     for (let i = 0; i < positions.length; i++) {
@@ -28,28 +39,25 @@ function setup() {
       if (d < textWidth("L")) {
         overlaps = true;
         break;
-      } else if (d < textWidth("O")){
+      } else if (d < textWidth("O")) {
         overlaps = true;
         break;
-      } else if (d < textWidth("V")){
+      } else if (d < textWidth("V")) {
         overlaps = true;
         break;
-      } else if (d < textWidth("E")){
+      } else if (d < textWidth("E")) {
         overlaps = true;
         break;
-      }  
+      }
     }
-    // If the position does not overlap, draw the letter and store the position
-    if (!overlaps) {
-      text(random_item(items), x, y);
-      positions.push({ x, y });
-    }
-    // Reset the rotation
+    text(random_item(items), x, y);
+    positions.push({ x, y });
     rotate(-angle);
   }
 }
 
-function mouseClicked() {
-  // Save as an image
-  save("letters.jpg");
+function keyPressed() {
+  if (key === 's') {
+    saveCanvas('maximalism', 'jpg');
+  }
 }
