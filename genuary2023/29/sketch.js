68f0b7ce052298862fb9e5045ae7badea15d29d3
Suraj Rai <surajrajrai@gmail.com>
2023-01-29 18:41:26 +0000

    genuary

--- genuary2023/29/sketch.js
+++ genuary2023/29/sketch.js
@@ -1,25 +1,39 @@
+let colors = ['#FDA7DF', '#F79F1F', '#009432', '#B53471', '#A3CB38', '#12CBC4', '#D980FA', '#0652DD'];
+
 function random_item(items) {
   return items[Math.floor(Math.random() * items.length)];
 }
 
 function setup() {
+  noStroke();
   rectMode(CENTER)
   textAlign(CENTER)
   createCanvas(600, 600);
   background('#EA2027');
-  frame1Love(100, 100, 150);
-  for (let i = 0; i < width; i += 150) {
-    for (let b = 0; b < height; b += 150) {
-      frame1Love(i, b, random(100,150));
-    }
-  }
+
+  frame1(100, 100, 150);
+  frame2(300, 100, 150);
+  frame3(500, 100, 150);
+
+  frame2(100, 300, 150);
+  frame1(330, 340, 250);
+
+  frame3(100, 500, 130);
+  frame3(400, 540, 90);
+
+  frame2(540, 280, 100);
+  frame4(540, 420, 100);
 }
 
-function frame1Love(xpos, ypos, dimensions) {
-  let positions = [];
+//love
+function frame1(xpos, ypos, dimensions) {
+  //shadow
+  fill(100);
+  rect(xpos - 10, ypos - 10, dimensions + 5)
+
   let items = ['L', 'O', 'V', 'E'];
   let letterSize = 30;
-  fill('#FDA7DF')
+  fill(random_item(colors))
   rect(xpos, ypos, dimensions - 3)
   //contents
   noFill();
@@ -27,11 +41,63 @@ function frame1Love(xpos, ypos, dimensions) {
   textSize(letterSize);
   for (let i = 0; i < 28; i++) {
     fill(255);
-    let angle = random(-180, 180);
-    let x = random(xpos - (letterSize * 2), xpos + (letterSize * 2));
-    let y = random(ypos - (letterSize * 2), ypos + (letterSize * 2));
+    let x = random(xpos - (dimensions / 2.5), xpos + (dimensions / 2.5));
+    let y = random(ypos - (dimensions / 2.5), ypos + (dimensions / 2.5));
     text(random_item(items), x, y);
-    positions.push({ x, y });
+  }
+  strokeWeight(15);
+  //frame
+  noFill();
+  stroke('#5758BB');
+  rect(xpos, ypos, dimensions)
+
+  strokeWeight(1);
+}
+//xyz
+function frame2(xpos, ypos, dimensions) {
+  //shadow
+  fill(100);
+  rect(xpos - 10, ypos - 10, dimensions + 5)
+
+  fill(random_item(colors))
+  rect(xpos, ypos, dimensions - 3)
+  //contents
+  noFill();
+  stroke(255);
+  for (let i = 0; i < 5; i++) {
+    ellipse(xpos-i*2, ypos , dimensions / 2)
+    ellipse(xpos+ i*2, ypos , dimensions / 2)
+    ellipse(xpos, ypos - i*2, dimensions / 2)
+    ellipse(xpos, ypos + i*2, dimensions / 2)
+    ellipse(xpos, ypos, dimensions / 2)
+  }
+  //frame
+  strokeWeight(15);
+  noFill();
+  stroke('#5758BB');
+  rect(xpos, ypos, dimensions)
+  strokeWeight(1);
+}
+
+//shapes
+function frame3(xpos, ypos, dimensions) {
+  //shadow
+  fill(100);
+  rect(xpos - 10, ypos - 10, dimensions + 5)
+
+  let items = ['X', 'O', 'X'];
+  let letterSize = random(20, 30);
+  fill(random_item(colors))
+  rect(xpos, ypos, dimensions - 3)
+  //contents
+  noFill();
+  stroke(255);
+  textSize(letterSize);
+  for (let i = 0; i < 5; i++) {
+    fill(255);
+    let x = random(xpos - (dimensions / 2.5), xpos + (dimensions / 2.5));
+    let y = random(ypos - (dimensions / 2.5), ypos + (dimensions / 2.5));
+    text(random_item(items), x, y);
   }
   strokeWeight(15);
   //frame
@@ -41,29 +107,25 @@ function frame1Love(xpos, ypos, dimensions) {
   strokeWeight(1);
 }
 
-function frame2numbers(xpos, ypos, dimensions) {
-  let positions = [];
-  let items = ['1', '2', '3', '4'];
-  let letterSize = 20;
-  fill('#FDA7DF')
+function frame4(xpos, ypos, dimensions) {
+  //shadow
+  fill(100);
+  rect(xpos - 10, ypos - 10, dimensions + 5)
+
+  fill(random_item(colors))
   rect(xpos, ypos, dimensions - 3)
   //contents
   noFill();
   stroke(255);
-  textSize(letterSize);
-  for (let i = 0; i < 28; i++) {
-    fill(255);
-    let angle = random(-180, 180);
-    let x = random(xpos - (letterSize * 2), xpos + (letterSize * 2));
-    let y = random(ypos - (letterSize * 2), ypos + (letterSize * 2));
-    text(random_item(items), x, y);
-    positions.push({ x, y });
+  for (let i = 0; i < 10; i++) {
+    rect(xpos, ypos , (dimensions /3)+i*3  )
   }
-  strokeWeight(15);
   //frame
+  strokeWeight(15);
   noFill();
   stroke('#5758BB');
   rect(xpos, ypos, dimensions)
+  strokeWeight(1);
 }
 
 function keyPressed() {
